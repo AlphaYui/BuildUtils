d7875c6b66f3ce78fd5e72e15510529744fae002
marzipankaiser <marzipankaiser@gmail.com>
2015-02-15 22:11:16 +0100

    Add method to interface with Bukkit commands.

--- src/com/gmail/marzipankaiser/argumentreader/CommandLibrary.java
+++ src/com/gmail/marzipankaiser/argumentreader/CommandLibrary.java
@@ -72,6 +72,11 @@ public class CommandLibrary {
 		int i = command.indexOf(' ');
 		return execute(command.substring(0, i), command.substring(i+1));
 	}
+	public Object handleCommand(CommandSender cs, org.bukkit.command.Command cmd,
+			String label, String[] args){
+		setCommandSender(cs);
+		return execute(String.join(" ", args));
+	}
 	
 	
 	public void addCommand(Command cmd){

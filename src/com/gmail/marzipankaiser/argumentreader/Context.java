1e1356470e3af0f320a16f446eca564fa30d8ef2
marzipankaiser <marzipankaiser@gmail.com>
2015-02-15 19:48:07 +0100

    Change "Context" to allow for stacked arguments

--- src/com/gmail/marzipankaiser/argumentreader/Context.java
+++ src/com/gmail/marzipankaiser/argumentreader/Context.java
@@ -1,6 +1,12 @@
 package com.gmail.marzipankaiser.argumentreader;
 
+import java.util.Collection;
+
 public interface Context {
 	public Object get(String name);
 	public void set(String name, Object value);
+	
+	public void push(String name, Object value);
+	public Object pop(String name);
+	public Collection<Object> getAll(String name);
 }
--- src/com/gmail/marzipankaiser/argumentreader/HashMapContext.java
+++ src/com/gmail/marzipankaiser/argumentreader/HashMapContext.java
@@ -1,19 +1,39 @@
 package com.gmail.marzipankaiser.argumentreader;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Stack;
 
 public class HashMapContext implements Context {
 
-	public HashMap<String, Object> values;
+	public HashMap<String, Stack<Object>> values;
 	
 	@Override
 	public Object get(String name) {
-		return values.get(name);
+		return values.get(name).peek();
 	}
 
 	@Override
 	public void set(String name, Object value) {
-		values.put(name, value);
+		values.put(name, new Stack<Object>());
+		push(name,value);
+	}
+
+	@Override
+	public void push(String name, Object value) {
+		if(!values.containsKey(name))
+			values.put(name, new Stack<Object>());
+		values.get(name).push(value);
+	}
+
+	@Override
+	public Object pop(String name) {
+		return values.get(name).pop();
+	}
+
+	@Override
+	public Collection<Object> getAll(String name) {
+		return values.get(name);
 	}
 
 }

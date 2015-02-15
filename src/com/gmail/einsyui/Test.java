a8b1ab3bda15dc5199c113e5e4accdd51bd35a31
Einsyui <einsyui@gmail.com>
2015-02-15 18:55:17 +0100

    Added compiler errors.

---
+++ src/com/gmail/einsyui/Test.java
@@ -0,0 +1,17 @@
+package com.gmail.einsyui;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public boolean isItOnGitHub()
+	{
+		return true;
+	}
+	
+	public int weNeedMoreCompilerErrors( )
+	{
+		return new ArrayList<Test>();
+	}
+	
+}

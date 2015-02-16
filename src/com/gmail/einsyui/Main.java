90bfabcf4b3bb2245df7f43d8e23647d11975084
Einsyui <einsyui@gmail.com>
2015-02-16 20:25:32 +0100

    * Fixed errors occuring when using /bu set + Added function to Main.java to return certain location stacks --> Successfully tested BuildUtils the first time

--- src/com/gmail/einsyui/Main.java
+++ src/com/gmail/einsyui/Main.java
@@ -1,17 +1,9 @@
 package com.gmail.einsyui;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.command.RemoteConsoleCommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gmail.einsyui.commands.SetCommand;
@@ -44,89 +36,13 @@ public class Main extends JavaPlugin{
 	
 	public LocationStack getStackFor( String s )
 	{
-		return stacks.get( s );
-	}
-	
-	public static boolean isInt( String s )
-	{
-		try {
-		Integer.parseInt( s );
-		} catch( IllegalArgumentException e )
+		LocationStack locStack = stacks.get( s );
+		
+		if( locStack == null )
 		{
-			return false;
+			stacks.put( s, new LocationStack() );
+			return stacks.get( s );
 		}
-		
-		return true;
-	}
-	
-	public static int parseInt( String s )
-	{
-		int i=0;
-		
-		try {
-		i = Integer.parseInt( s );
-		} catch( IllegalArgumentException e )
-		{
-			return -1;
-		}
-		
-		return i;
-	}
-	
-	public static boolean isPlayer( CommandSender cs )
-	{
-		return (cs instanceof Player);
-	}
-	
-	public static boolean isBlock( CommandSender cs )
-	{
-		return (cs instanceof BlockCommandSender);
-	}
-	
-	public static boolean isConsole( CommandSender cs )
-	{
-		return ( cs instanceof ConsoleCommandSender || cs instanceof RemoteConsoleCommandSender );
-	}
-	
-	public static Location getCmdLocation( CommandSender cs )
-	{
-		if( isConsole( cs ) ) return null;
-		if( isBlock( cs ) )
-		{
-			BlockCommandSender bcs = (BlockCommandSender) cs;
-			return bcs.getBlock().getLocation();
-		}
-		if( isPlayer( cs ) )
-			return castPlayer( cs ).getLocation();
-		
-		return null;
-	}
-	
-	public static Player castPlayer( CommandSender cs )
-	{
-		return (Player) cs;
-	}
-	
-	public void showGeneralHelp()
-	{
-		//TODO
-	}
-	
-	public List<String> getHelpFor( String search )
-	{
-		Map<String,List<String>> help = getHelpMap();
-		List<String> result = new ArrayList<String>();
-		
-		if( !help.containsKey( search.toLowerCase() ) )
-			result.add( ChatColor.RED + "There's no help for that command. Was it written correctly?" );
-		
-		return help.get( search.toLowerCase() );
-	}
-	
-	public Map<String,List<String>> getHelpMap()
-	{
-		Map<String,List<String>> help = new HashMap<String,List<String>>();
-		
-		return help;
+		else return locStack;
 	}
 }
